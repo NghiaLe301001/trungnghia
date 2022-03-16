59e616d16ec3fae53e82fa007a3feda1341dc56f
NghiaLe301001 <ltn301001@gmail.com>
2022-03-16 15:19:26 +0700

    first commit

---
+++ Tuan4_LeTrungNghia/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Tuan4_LeTrungNghia
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
